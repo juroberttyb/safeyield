f690163aab33dea26f68de1fccf11c7657f174c3
r <juroberttyb@gmail.com>
2025-02-04 16:22:33 +0800

    update: ui

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ While DeFi allows anyone to participate safely without needing to trust intermed
 Despite offering greater security and higher yields—thanks to the removal of middlemen like banks—the fear of being scammed by unknown entities outweighs the potential benefits.
 
 However, they trust us. <b>We can bridge this gap by providing a simple and reliable entrance to DeFi for them</b>—through us, the crypto bros.
-
-This repository is secured by 'Trust Me, Bro'. It should only be used when hosted by someone you trust.
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,59 @@
 use leptos::*;use leptos::prelude::*;
+use web_sys;
 
 #[component]
-fn App(increment: i32) -> impl IntoView {
-    let (count, set_count) = signal(0);
+fn App() -> impl IntoView {
+    let deposit_address = "0xa0fE0fFA1234cf1f9Ee198De2d6A3BA7A23Cd874";
+    let (deposit_address_getter, _) = signal(deposit_address.to_string());
 
     view! {
-    <div class="container">
+        <div class="container">
 
-        <img
-            id="logo"
-            // class="rss opacity"
-            loading="lazy"
-            src="./images/eth.png"
-            alt=""
-            height="300"
-            width="300"
-        />
+            <img
+                id="logo"
+                // class="rss opacity"
+                loading="lazy"
+                src="./images/eth.png"
+                alt=""
+                height="300"
+                width="300"
+            />
 
-        <h1>"0xa0fE0fFA1234cf1f9Ee198De2d6A3BA7A23Cd874"</h1>
-        <h2><i>"ETH, USDT, USDC"</i></h2>
+            <div class="paragraph">
+                <div class="align-left">
+                    <p class="upper">"從Max交易所,使用Arbitrum網路,提領ETH,USDT,USDC中任一種到這個地址"</p>
+                    <p class="center-text upper">{ move || deposit_address_getter.get() }</p>
+                    <div class="center upper lower">
+                        <button
+                            class="button"
+                            on:click= move |_| {
+                                let window = web_sys::window().unwrap();
+                                let navigator = window.navigator();
+                                let clipboard = navigator.clipboard();
+                                let _ = clipboard.write_text(deposit_address);
+                            }
+                        >
+                            複製地址
+                        </button>
+                    </div>
+                </div>
+            </div>
 
-        <button
-            on:click= move |_| {
-                set_count.set(count.get() + increment)
-            }
-        >
-            "Click me: "
-            {count}
-        </button>
+            <div class="paragraph">
+                <div class="align-left">
+                    <p>"這邊的收益 (每日更新): ETH (1.7%), USDC (5.5%), USDT (5.1%)"</p>
+                    <p><a href="https://max.maicoin.com/docs/yield">"Max的收益 (每日更新): ETH (0.8%), USDC (1.0%), USDT (1.38%)"</a></p>
+                </div>
+            </div>
 
-
-    </div>
+        </div>
     }
 }
 
 fn main() {
     mount_to_body(|| {
         view! {
-            <App increment=5 />
+            <App />
         }
     })
 }
